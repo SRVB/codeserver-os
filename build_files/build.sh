67559e511fd453dd6b01f6056e8417964c47bb0d
Friesi <friesi@srvb.at>
2026-09-08 09:20:15 +0000

    Refactor build script to remove commented package installation examples and streamline installer execution

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,21 +7,10 @@ cp -avf "/ctx/system_files"/. /
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+mapfile -t installers < <(find /ctx/install -maxdepth 1 -name '*.sh' | LC_ALL=C sort -V)
+for installer in "${installers[@]}"; do
+	printf 'Running %s\n' "${installer}"
+	bash "${installer}"
+done
 
 systemctl enable podman.socket
